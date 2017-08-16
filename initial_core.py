6afe578ab5cd52cdf513fd3896814ee9f0d1f5b0
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-08-16 11:15:56 -0700

    what the hell is going on in the book

--- initial_core.py
+++ initial_core.py
@@ -80,9 +80,9 @@ def initialize_vel_press(config, initial_x_vel, initial_y_vel, initial_pressure)
     #         initial_pressure is initial initial pressure
     # output: velocities in x- and y- directions and pressure on all
     #         interior grid points.
-    x_velocities = initial_x_vel * np.ones(config.imax+2, config.jmax+2)
-    y_velocities = initial_y_vel * np.ones(config.imax+2, config.jmax+2)
-    pressures = initial_pressure * np.ones(config.imax+2, config.jmax+2)
+    x_velocities = initial_x_vel * np.ones(config.imax-1, config.jmax-1)
+    y_velocities = initial_y_vel * np.ones(config.imax-1, config.jmax-1)
+    pressures = initial_pressure * np.ones(config.imax-1, config.jmax-1)
     return x_velocities, y_velocities, pressures
     
 """
